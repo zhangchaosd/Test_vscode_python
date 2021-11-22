f54060351eed42aa8fe9f3802e706c26abc06527
zhangchao <zhangchaogoo@gmail.com>
2021-11-22 21:33:07 +0800

    update

--- core/first.py
+++ core/first.py
@@ -1,24 +1,24 @@
 import os
 import sys
 import numpy as np
-sys.path.append('..')
-# from utils.addd import addd
+sys.path.append('.')
+from utils.addd import addd
 def ns(data):
-        return normalization(standardization(data))
+    return normalization(standardization(data))
+
 def normalization( data):
-        _range = np.max(abs(data))
-        return data / _range
+    _range = np.max(abs(data))
+    return data / _range
 
 def standardization(data):
-        mu = np.mean(data, axis=0)
-        sigma = np.std(data, axis=0)
-        return (data - mu) / sigma
+    mu = np.mean(data, axis=0)
+    sigma = np.std(data, axis=0)
+    return (data - mu) / sigma
 # -b 8
 if __name__ == "__main__":
-    
-    print(os.getcwd())
-    # print(addd(12,9))
-    print(sys.path)
+#     print(os.getcwd())
+    print(addd(12,9))
+#     print(sys.path)
 
 
     sz1 = [1., 2.]
